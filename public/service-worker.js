7735101400e78fd190382e79d42fc1bb114dc13d
Anitha Venkatesan <anithamca68@gmail.com>
2020-08-05 16:37:01 -0500

    lint error fixed

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,31 +1,31 @@
 
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/manifest.webmanifest',
-    '/assets/css/styles.css',
-    'assets/js/db.js', 
-    '/assets/js/index.js',
-    '/assets/images/icons/icon-192x192.png',
-    '/assets/images/icons/icon-512x512.png',
-  ];
-  
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v5";
-  
-  // install
-  self.addEventListener("install", function(evt) {
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll(FILES_TO_CACHE);
-      })
-    );
-  
-    self.skipWaiting();
-  });
-  
-self.addEventListener("activate", function(evt) {
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/assets/css/styles.css",
+  "assets/js/db.js",
+  "/assets/js/index.js",
+  "/assets/images/icons/icon-192x192.png",
+  "/assets/images/icons/icon-512x512.png",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v5";
+
+// install
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
@@ -42,7 +42,7 @@ self.addEventListener("activate", function(evt) {
   self.clients.claim();
 });
 // fetch
-self.addEventListener("fetch", function(evt) {
+self.addEventListener("fetch", function (evt) {
   // cache successful requests to the API
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
@@ -66,13 +66,13 @@ self.addEventListener("fetch", function(evt) {
     return;
   }
   // if the request is not for the API, serve static assets using "offline-first" approach.
-// see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-evt.respondWith(
-  caches.match(evt.request).then(function(response) {
-    return response || fetch(evt.request);
-  })
-);
+  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+  evt.respondWith(
+    caches.match(evt.request).then(function (response) {
+      return response || fetch(evt.request);
+    })
+  );
 });
 
 
-    
+
